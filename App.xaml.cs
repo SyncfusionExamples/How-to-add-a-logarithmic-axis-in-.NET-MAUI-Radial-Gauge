7014b7d3abf5de650e902fbb801be3b40f678b29
Ragul S V <ragul.subramani@syncfusion.com>
2022-04-01 00:10:57 +0530

    MAUI-1626: How to add a logarithmic scale in .NET MAUI Radial Gauge control (SfRadialGauge) -> Sample update

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomScale;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CustomScale;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
